b400096e6910861edd04bc138988a1af1a5d0af1
Myasnikov <myasnikovik@gmail.com>
2019-05-20 22:47:12 +0500

    test: Поиск мин. пары чисел в массиве, имеющем менее двух элементов

--- SmallestPairSumInArray.Tests/SearchEngineTests.cs
+++ SmallestPairSumInArray.Tests/SearchEngineTests.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpFunctionalExtensions;
+using System;
 using Xunit;
 
 namespace SmallestPairSumInArray.Tests
@@ -23,6 +24,19 @@ namespace SmallestPairSumInArray.Tests
             Assert.Equal(expected.Value, smallestPairResult.Value);
         }
 
+        [Theory]
+        [ClassData(typeof(ArraysWithLessThanOneElements))]
+        public void SearchSmallestPair_NumbersArrayWithLessThanTwoElements_ResultFailed(int[] input)
+        {
+            var expected = Result.Fail<(int, int)>("The array must contain at least two elements.");
+
+            var smallestPairResult = _searchEngine.SearchSmallestPair(input);
+
+            Assert.True(smallestPairResult.IsFailure);
+            Assert.Equal(smallestPairResult.Error, expected.Error);
+        }
+
+
         private class ArraysWithMoreThanOneElements : TheoryData<int[], Result<(int, int)>>
         {
             public ArraysWithMoreThanOneElements()
@@ -30,6 +44,15 @@ namespace SmallestPairSumInArray.Tests
                 Add(new int[] { 0, 1 }, Result.Ok((0, 1)));
                 Add(new int[] { 4, 0, 3, 19, 492, -10, 1 }, Result.Ok((-10, 0)));
                 Add(new int[100000000], Result.Ok((0, 0)));
+            }
+        }
+
+        private class ArraysWithLessThanOneElements : TheoryData<int[]>
+        {
+            public ArraysWithLessThanOneElements()
+            {
+                Add(Array.Empty<int>());
+                Add(new int[] { 0 });
             }
         }
     }
