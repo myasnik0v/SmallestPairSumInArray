47e2cb8b3a4fe470d9df8c6b41fa99cfbd57514f
Myasnikov <myasnikovik@gmail.com>
2019-05-20 22:17:32 +0500

    build: создал консольное приложение SmallestPairInArray

---
+++ SmallestPairSumInArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmallestPairSumInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
