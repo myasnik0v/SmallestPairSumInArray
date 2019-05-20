2209051260479f029b720d9b13915ff7bb6a4388
Myasnikov <myasnikovik@gmail.com>
2019-05-20 22:39:04 +0500

    feat: класс SearchEngine с методом поиска пары наименьших чисел

--- SmallestPairSumInArray/SearchEngine.cs
+++ SmallestPairSumInArray/SearchEngine.cs
@@ -6,4 +6,33 @@ namespace SmallestPairSumInArray
     {
         Result<(int, int)> SearchSmallestPair(int[] input);
     }
+
+    public class SearchEngine : ISearchEngine
+    {
+        /// <summary>
+        /// Ищет два минимальных элемента массива
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public Result<(int, int)> SearchSmallestPair(int[] input)
+        {
+            int first = int.MaxValue;
+            var second = int.MaxValue;
+
+            foreach (var item in input)
+            {
+                if (item < first)
+                {
+                    second = first;
+                    first = item;
+                }
+                else if (item < second)
+                {
+                    second = item;
+                }
+            }
+
+            return Result.Ok((first, second));
+        }
+    }
 }
