ff03284fb9c2c858654e6c1c94e2119cc1c43701
Myasnikov <myasnikovik@gmail.com>
2019-05-20 23:07:51 +0500

    feat: добавил класс, возвращающий сумму двух мин. эл. массива

---
+++ SmallestPairSumInArray/ArrayHelper.cs
@@ -0,0 +1,32 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+
+namespace SmallestPairSumInArray
+{
+    public class ArrayHelper
+    {
+        private readonly ISearchEngine _searchEngine;
+
+        public ArrayHelper(ISearchEngine searchEngine)
+        {
+            _searchEngine = searchEngine ?? throw new ArgumentNullException(nameof(searchEngine));
+        }
+
+        /// <summary>
+        /// Возвращает сумму двух минимальных элементов массива
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public Result<int> SumOfSmallestPair(int[] input)
+        {
+            var smallestPairResult = _searchEngine.SearchSmallestPair(input);
+
+            if (smallestPairResult.IsFailure)
+                return Result.Fail<int>(smallestPairResult.Error);
+
+            var sumOfSmallestPair = smallestPairResult.Value.Item1 + smallestPairResult.Value.Item2;
+
+            return Result.Ok(sumOfSmallestPair);
+        }
+    }
+}
