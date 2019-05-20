a7022eb0208a3d6235a7ab6893965cd553458c1d
Myasnikov <myasnikovik@gmail.com>
2019-05-20 22:58:37 +0500

    feat: добавил ValuesProvider
    immutable-класс для ленивого обхода массива и возможности подменить input[] - файлом, например.

---
+++ SmallestPairSumInArray/ValuesProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmallestPairSumInArray
+{
+    public interface IValuesProvider
+    {
+        IEnumerable<int> Values { get; }
+        int ItemsCount { get; }
+    }
+
+    public class ValuesProvider : IValuesProvider
+    {
+        private readonly int[] _array;
+
+        private ValuesProvider(int[] input)
+        {
+            _array = input ?? throw new ArgumentNullException(nameof(input));
+        }
+
+        public static ValuesProvider Create(int[] input)
+        {
+            return new ValuesProvider(input);
+        }
+
+        public IEnumerable<int> Values
+        {
+            get
+            {
+                foreach (var item in _array)
+                    yield return item;
+            }
+        }
+
+        public int ItemsCount => _array.Length;
+    }
+}
